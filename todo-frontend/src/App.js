524c65f1c526dbe5fc46d5b0a66ffcfca17e5c5b
ThomasMuller30 <104437879+ThomasMuller30@users.noreply.github.com>
2025-07-01 22:55:15 +0200

    Ajout de fetch dans App.js
    - Utilisation de fetch pour utiliser les routes de récupération et d'ajout de taches
    - Ajout d'un html rapide

--- todo-frontend/src/App.js
+++ todo-frontend/src/App.js
@@ -1,8 +1,40 @@
-function App() {
-  return (
-    <>
+import { useState, useEffect } from 'react';
 
-    </>
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [title, setTitle] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000/todos')
+      .then(res => res.json())
+      .then(data => setTodos(data));
+  }, []); //Pour voir la liste
+
+  const addTodo = () => {
+    fetch('http://localhost:5000/todos', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ title })
+    })
+    .then(res => res.json())
+    .then(newTodo => {
+      setTodos([...todos, newTodo]);
+      setTitle('');
+    }); //Pour faire une nouvelle tache
+  };
+
+  return (
+    <div>
+      <h1>Choses à faire</h1>
+      <input value={title} onChange={e => setTitle(e.target.value)} />
+      <button onClick={addTodo}>Ajouter</button>
+
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
